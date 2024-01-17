b4a450d0bd2c21e2965442846ebc97d84894ea63
Kazuya-01 <msyarifudin352@gmail.com>
2024-01-17 19:33:15 +0700

    config

---
+++ db_config.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root"; // Ganti dengan username database Anda
+$password = ""; // Ganti dengan password database Anda
+$dbname = "db_karyawan"; // Ganti dengan nama database Anda
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
